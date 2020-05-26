3b73e9db4131a12ac03ae948c8a1c3d1567b0dba
ZakariaBrahimi <56256409+ZakariaBrahimi@users.noreply.github.com>
2020-05-26 17:53:07 +0100

    fix problem

--- env/Scripts/src/mid_app/urls.py
+++ env/Scripts/src/mid_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 app_name = 'mid_app'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^doctors/$', views.doctors, name='doctors'),
+    url(r'^services/$', views.services, name='services'),
+    url(r'^blog/$', views.blog, name='blog'),
 ]
--- env/Scripts/src/mid_app/views.py
+++ env/Scripts/src/mid_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'index.html', context)
+
+def doctors(request):
+    context = {}
+    return render(request, 'doctor.html', context)
+
+def services(request):
+    context = {}
+    return render(request, 'services.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'blog.html', context)
